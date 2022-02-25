8293d051a34cc2bfbd84eb7e1cea78834956a94c
SUZUKI Kei <todokimasen@gmail.com>
2022-02-25 17:49:44 +0900

    現在のページを Google 翻訳するブックマークレットを作成した.

---
+++ src/gt.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * 現在のページを Google 翻訳する.
+ *
+ */
+
+(function(){
+    window.location.href = "https://translate.google.com/translate?sl=en&tl=ja&u=" + encodeURI(window.location.href)
+})()
+
---
+++ test/test.gt.js
@@ -0,0 +1,29 @@
+const assert = require("assert")
+const fs = require("fs")
+
+function gt() {
+    eval(fs.readFileSync("./src/gt.js", "utf-8"))
+}
+
+describe("gt", () => {
+
+    beforeEach(() => {
+        global.window = {location: {href: null}}
+    })
+
+    const tests = [
+        {
+            url: "https://example.com",
+            expected: "https://translate.google.com/translate?sl=en&tl=ja&u=https://example.com",
+        },
+    ]
+    tests.forEach(({url, expected}, i) => {
+        it("#" + i, () => {
+            global.window.location.href = url
+            gt()
+            assert.equal(expected, global.window.location.href)
+        })
+    })
+
+})
+
