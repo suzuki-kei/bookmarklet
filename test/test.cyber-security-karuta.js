aebf61171c2d3411ea31190a84d020a1e106c099
SUZUKI Kei <todokimasen@gmail.com>
2022-03-06 11:13:16 +0900

    cyber-security-karuta.html のテストを作成した.

---
+++ test/test.cyber-security-karuta.js
@@ -0,0 +1,25 @@
+const assert = require("assert")
+const fs = require("fs")
+
+function cyber_security_karuta() {
+    html = fs.readFileSync("./src/cyber-security-karuta.html", "utf-8")
+    javascript = html.replace(/^(?:.|\n)*<script[^>]*>\n*([^<]*?)\n*<\/script>(?:.|\n)*$/gi, "$1")
+    eval(javascript)
+}
+
+describe("cyber-security-karuta", () => {
+
+    beforeEach(() => {
+        global.window = {location: {href: null}}
+        global.location = global.window.location
+    })
+
+    it("#1", () => {
+        cyber_security_karuta()
+        assert.match(
+            global.window.location.href,
+            /https:\/\/www\.ipa\.go\.jp\/security\/otasuketai-pr\/assets\/img\/karuta\/[a-z]+\.png$/)
+    })
+
+})
+
