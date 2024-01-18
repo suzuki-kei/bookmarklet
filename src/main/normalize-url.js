b16283e66075fea829eec3928d54dae1faa86bd3
SUZUKI Kei <todokimasen@gmail.com>
2024-01-19 08:49:41 +0900

    使わないサービスをサポートしないように変更した.

--- src/main/normalize-url.js
+++ src/main/normalize-url.js
@@ -25,9 +25,6 @@
  *     https://twitter.com/suzuki6b6569/status/1480061968547725316?s=21
  *     -> https://twitter.com/suzuki6b6569/status/1480061968547725316
  *
- *     https://www.monotaro.com/g/00542161/?t.q=%E3%82%B3%E3%83%94%E3%83%BC%E7%94%A8%E7%B4%99
- *     -> https://www.monotaro.com/g/00542161/
- *
  */
 
 (function(){
@@ -39,8 +36,6 @@
     url = url.replace(/^(https:\/\/www\.amazon\.co\.jp)\/(?:.*\/)product-reviews\/([0-9a-zA-Z]+).*$/, "$1/product-reviews/$2")
     url = url.replace(/^(https:\/\/twitter\.com\/[-_0-9a-zA-Z]+\/status\/[0-9]+).*$/, "$1")
     url = url.replace(/^(https:\/\/qiita\.com\/[-_0-9a-zA-Z]+\/items\/[0-9a-zA-Z]+).*$/, "$1")
-    url = url.replace(/^(https:\/\/www\.monotaro\.com\/g\/[0-9]+).*$/, "$1/")
-    url = url.replace(/^(https:\/\/www\.monotaro\.com\/p\/[0-9]+\/[0-9]+).*$/, "$1/")
     location.href = url;
 })()
 
